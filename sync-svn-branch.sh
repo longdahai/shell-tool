1c8c84b6290be831896b6844f01f9bd2e4931fc6
dahai <haiwei.free@gmail.com>
2017-03-06 17:21:37 +0800

    Add verion choose Add confirm

--- sync-svn-branch.sh
+++ sync-svn-branch.sh
@@ -20,7 +20,12 @@ svn revert *
 svn cleanup --remove-unversioned
 cd $gitDir
 
-syncFile=$(git diff master.. --name-only)
+# if has --all, or else last change
+if [[ $1 = '--all' ]]; then
+    syncFile=$(git diff master.. --name-only)
+else
+    syncFile=$(git diff -1 --name-only)
+fi
 for file in $syncFile
 do
   cp $file $svnDir/$file
@@ -29,6 +34,11 @@ done
 # svn commit before check
 cd $svnDir
 svn status
+
+read -p "svn confirm commit? (y/n)" yorn
+if [[ $yorn = 'n' ]]; then
+  exit 0
+fi
 
 # svn commit
 svn add *
