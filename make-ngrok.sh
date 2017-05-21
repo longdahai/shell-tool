940f6b046faf2340d6ca737490d0c3df3068cb22
haiwei long <haiwei.free@gmail.com>
2017-05-21 21:40:04 +0800

    add ngrok

---
+++ make-ngrok.sh
@@ -0,0 +1,11 @@
+export NGROK_DOMAIN="域名"
+
+openssl genrsa -out rootCA.key 2048
+openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$NGROK_DOMAIN" -days 5000 -out rootCA.pem
+openssl genrsa -out server.key 2048
+openssl req -new -key server.key -subj "/CN=$NGROK_DOMAIN" -out server.csr
+openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 5000
+
+cp rootCA.pem assets/client/tls/ngrokroot.crt
+cp server.crt assets/server/tls/snakeoil.crt
+cp server.key assets/server/tls/snakeoil.key
