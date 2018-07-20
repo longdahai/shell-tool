b8c80da3d0fe3ecfa6920ff1e9c28f077ec69807
LongHaiwei <86127779@qq.com>
2018-07-20 15:48:46 +0800

    update install debian9

--- git/create-shortcut.sh
+++ git/create-shortcut.sh
@@ -5,3 +5,5 @@ git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.br branch
 git config --global alias.rs reset
+
+git config --global alias.fh flow hotfix
--- install-atom.sh
+++
@@ -1,4 +0,0 @@
-curl -OL https://atom.io/download/deb
-dpkg -i deb
-apm config set registry http://registry.npm.taobao.org
-
