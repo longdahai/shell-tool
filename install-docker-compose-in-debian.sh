68bb691c0ac666219e601a3f89c0a644d9e2b509
Haiwei Long <haiwei.free@gmail.com>
2018-12-03 17:42:16 +0800

    Update install-docker-compose-in-debian.sh

--- install-docker-compose-in-debian.sh
+++ install-docker-compose-in-debian.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
 
-apt-get remove docker docker-engine docker.io
 apt-get update
-
-apt-get install \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     gnupg2 \
-     software-properties-common -y
-
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-
-apt-get update
-
-apt-get install docker-ce -y
-
 apt-get install curl -y
 
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
