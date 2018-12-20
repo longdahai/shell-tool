b5810a327b0821982d958117b873ba2e644289aa
LongHaiwei <86127779@qq.com>
2018-12-20 17:34:01 +0800

    update

--- install-docker-compose-in-debian.sh
+++ install-docker-compose-in-debian.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-apt-get update
-apt-get install curl -y
+curl -fsSL https://get.docker.com | bash -
 
 curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
