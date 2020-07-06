7916fd84d288b5ccc4d568c51267619cb871dc08
Haiwei Long <haiwei.free@gmail.com>
2020-07-06 17:06:45 +0800

    Update install-docker.sh
    update docker-compose

--- install-docker.sh
+++ install-docker.sh
@@ -2,5 +2,5 @@
 
 curl -fsSL https://get.docker.com | bash -
 
-curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
